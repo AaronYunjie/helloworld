1a6f953b6fd610c4f4645fe8fef2c94d63a2d5d5
renyunjie <renyunjie@smartdevices.com.cn>
2016-08-20 14:06:25 +0800

    simple example

---
+++ hello/hello.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年08月20日 星期六 13时55分36秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argv, char *argc[])
+{
+        printf("hello world\n");
+        return 0;
+}
